f4e631a15cc0842ee52db49530dbd795577d5b50
7London7 <106383366+7London7@users.noreply.github.com>
2023-11-13 21:25:46 +0300

    111

---
+++ case.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include "case.h"
+using namespace std;
+
+long long itc_len(string str){
+    int i = 0;
+    int k = 0;
+    while(str[i] != '\0'){
+        k = k + 1;
+    i++;
+    }
+    return k;
+}
+
+string itc_probdop(int num1, int bit,int code){
+    int  d = 1, per, kol = 0, i = 0, ost;
+    char number;
+int num;
+string str = "";
+if(num1 < 0){
+num = -1 * num;
+}else{
+num = num1;
+}
+while(num > 0){
+   number = num % 2 + '0';
+   str = number + str;
+    num = num / 2;
+}
+if(code == 3){
+    while(str[i] != '\0'){
+           if(str[i] == '1'){
+        str[i] = '0';
+    }else{
+    str[i] = '1';
+    }
+    i++;
+    }
+}
+kol = itc_len(str);
+ost = bit - kol;
+while(ost > 0){
+    str = '0' + str;
+    --ost;
+}
+if(num1 < 0){
+    str[0] = '1';
+}else{
+str[0] = '0';
+}
+if(code == 1){
+    return str;
+}
+if(code == 2){
+ while(str[i] != '\0'){
+    if(str[i] == '1'){
+        str[i] = '0';
+    }else{
+    str[i] = '1';
+    }
+    i++;
+ }
+}
+}
---
+++ case.h
@@ -0,0 +1,8 @@
+#ifndef CASE_H_INCLUDED
+#define CASE_H_INCLUDED
+#include <iostream>
+#include <string>
+using namespace std;
+string itc_probdop(int num, int bit, int code);
+long long itc_len(string str);
+#endif // CASE_H_INCLUDED
