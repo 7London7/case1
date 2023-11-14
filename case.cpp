835da6115819b0e4fe685dfa24f6be7a05f9a5b5
7London7 <106383366+7London7@users.noreply.github.com>
2023-11-14 20:45:33 +0300

    123

--- case.cpp
+++ case.cpp
@@ -12,16 +12,77 @@ long long itc_len(string str){
     }
     return k;
 }
-
 string itc_probdop(int num1, int bit,int code){
-    int  d = 1, per, kol = 0, i = 0, ost;
-    char number;
+    int  d = 1, per, kol = 0, i = 0, ost, t, b = 1;
+    char number, a;
+    char l;
 int num;
 string str = "";
 if(num1 < 0){
 num = -1 * num1;
 }else{
 num = num1;
+}
+if(code == 3){
+    while(num > 0){
+   t = t + (num % 2) * b;
+    num = num / 2;
+    b = b * 10;
+}
+   t = t + 1;
+   a = t + '0';
+    str = a + str;
+    kol = itc_len(str);
+ost = bit - kol;
+while(ost > 0){
+    str = '0' + str;
+    --ost;
+}
+    while(str[i] != '\0'){
+    if(str[i] == '1'){
+        str[i] = '0';
+    }else{
+    str[i] = '1';
+    }
+    i++;
+ }
+ if(num1 < 0){
+    str[0] = '1';
+}else{
+str[0] = '0';
+}
+int n, sum = 0;
+char ch[n];
+for(i = n - 1; i > -1; i--){
+   if(ch[i] == '1');
+   sum = sum + 2 * i;
+}
+sum = sum + 1;
+while(sum > 0){
+   l = sum % 2 + '0';
+   str = l + str;
+    sum = sum / 2;
+}
+kol = itc_len(str);
+ost = bit - kol;
+while(ost > 0){
+    str = '0' + str;
+    --ost;
+}
+ while(str[i] != '\0'){
+    if(str[i] == '1'){
+        str[i] = '0';
+    }else{
+    str[i] = '1';
+    }
+    i++;
+ }
+if(num1 < 0){
+    str[0] = '1';
+}else{
+str[0] = '0';
+}
+return str;
 }
 while(num > 0){
    number = num % 2 + '0';
@@ -51,6 +112,11 @@ if(code == 2){
     }
     i++;
  }
+ if(num1 < 0){
+    str[0] = '1';
+}else{
+str[0] = '0';
+}
  return str;
 }
 }
