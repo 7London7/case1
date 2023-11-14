6fbceada1c6e937f540afdf832b31f4d0789a868
7London7 <106383366+7London7@users.noreply.github.com>
2023-11-14 18:51:30 +0300

    1

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "case.h"
+using namespace std;
+
+int main()
+{
+    int ch, bit, m;
+    cin >> ch >> bit >> m;
+    cout << itc_probdop(ch, bit, m);
+    return 0;
+}
